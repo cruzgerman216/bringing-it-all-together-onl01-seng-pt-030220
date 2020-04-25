cfdba1a789aec6441a144575a0b3d5d8ee4d9dc3
German Cruz <cruzgerman216@yahoo.com>
2020-04-25 18:13:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,4 +48,13 @@ class Dog
     dog = Dog.new(id: row[0], name:row[1], breed: row[2])
     dog
   end
+
+  def self.find_by_id(id)
+    sql = <<-SQL 
+    SELECT * FROM dogs WHERE id = ? 
+    SQL
+
+    dog = DB[:conn].execute(sql, id)
+    dog
+  end
 end
