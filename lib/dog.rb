04d7fb8f83a99a37f012fe640a197b7d06ffe7a6
German Cruz <cruzgerman216@yahoo.com>
2020-04-25 18:05:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,11 +7,11 @@ class Dog
     @breed = breed
   end
 
-  def self.create_table 
-    sql = <<-SQL 
+  def self.create_table
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       breed TEXT
     )
     SQL
